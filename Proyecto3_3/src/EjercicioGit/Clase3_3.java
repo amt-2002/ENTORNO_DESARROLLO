be6fe8f66283c266e7cf4beac98e1c57f66101d2
Pelucaas <71947982+amt-2002@users.noreply.github.com>
2024-11-01 20:53:33 +0100

    x

--- Proyecto3_3/src/EjercicioGit/Clase3_3.java
+++ Proyecto3_3/src/EjercicioGit/Clase3_3.java
@@ -5,7 +5,7 @@ public class Clase3_3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Practica 3_3 + Angel Melendez Tena");
+		System.out.println("Canela en Rama");
 	}
 
 }
